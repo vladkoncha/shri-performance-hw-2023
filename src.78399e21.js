ec289ee3da43ef65c09eee41664f70435ab28547
Vlad <vla.koncha@gmail.com>
2023-07-05 00:40:02 +0700

    Updates

--- src.78399e21.js
+++ src.78399e21.js
@@ -29472,7 +29472,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64906" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51481" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
